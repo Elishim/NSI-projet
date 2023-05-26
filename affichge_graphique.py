44f3e6474cc666d60b650a92ec7126311edf1d6b
Elishim <96025392+Elishim@users.noreply.github.com>
2023-05-26 21:18:51 +0200

    Add files via upload

---
+++ affichge_graphique.py
@@ -0,0 +1,26 @@
+'''
+Eliot Finez 1A
+Projet NSI
+TSP
+'''
+
+import matplotlib.pyplot as plt
+
+Liste_test=[('Tokyo',1,2),('Paris',4,6),('New-York',9,-3),('Malaga',6,-8)]
+
+def plotting_ville(Liste):
+    '''
+    Parametre : Un liste de tuples sous la forme : (Nom de la ville , X , Y , Z )
+    exemple de liste : [('Tokyo',1,2,3),('Paris',4,6,-3),('New-York',9,-3,6),('Malaga',6,-8,7)]
+    Renvoie : None , affiche dans un espace 3D les villes via la bibliotheque matplotlib .
+    '''
+    Liste.append(Liste[0])
+    for i in range(len(Liste)-1):
+        plt.scatter(Liste[i][1],Liste[i][2], color='black')
+        plt.text(Liste[i][1],Liste[i][2],Liste[i][0], color='black', fontsize=12)
+        plt.plot([Liste[i][1],Liste[i+1][1]],[Liste[i][2],Liste[i+1][2]], color='blue')
+    plt.title("Carte des villes")
+    plt.show()
+    return None
+
+plotting_ville(Liste_test)
---
+++ glouton.py
@@ -0,0 +1,123 @@
+import math as m
+import matplotlib.pyplot as plt
+
+MAX_NB_VILLES = 500
+
+'''
+Petit fichier test fait par mes soins :
+
+26
+
+a 1 1
+b 6 8
+c -3 -2
+d 2 -4
+e 4 3
+f 8 10
+g -3 -3
+h -5 5
+i -1 7
+j 0 0
+k 1 9
+l 1 -7
+m 3 0
+n 1 2
+o 4 4
+p -6 6
+q -2 7
+r 1 -8
+s -2 8
+t 3 -1
+u -4 1
+v 1 -5
+w 9 2
+x -4 -9
+y -8 8
+z 8 -8
+
+
+'''
+
+
+def litEntree():
+    """
+    lit le nombre de villes, leur nom et leurs coordonnées
+    """
+
+    dist = [[0 for _ in range(MAX_NB_VILLES)] for _ in range(MAX_NB_VILLES)]
+    coord = [[0 for i in range(2)] for _ in range(MAX_NB_VILLES)]
+    noms = ["" for _ in range(MAX_NB_VILLES)]
+
+    nbV = int(input("Entrez le nombre de villes : "))
+    print("Entrez les noms des villes puis leurs coordonnées (entières, dans le plan) dans l'ordre:\n")
+    for i in range(nbV):
+        ligne = list(input().split())
+        noms[i] =ligne[0]
+        coord[i][0] = int(ligne[1])
+        coord[i][1] = int(ligne[2])
+    
+    #précalcule les distances entre les paires de villes
+    for i in range(MAX_NB_VILLES):
+        for j in range(MAX_NB_VILLES):
+            dist[i][j] = m.sqrt((coord[i][0] - coord[j][0])**2 + (coord[j][1] - coord[i][1])**2)
+    return nbV, coord, noms, dist
+
+def glouton(nbV, coord, dist):
+    """
+    construit un ordre des villes avec un algorithme glouton
+    renvoie un tuple : un tableau d'entiers (l'ordre des villes sur le chemin) et la distance totale
+    """
+
+    tot = 0 #distance totale
+    chem = [-1 for _ in range(nbV)]
+    chem[0] = 0
+    pris = [False for _ in range(nbV)]
+    pris[0] = True
+    for idV in range(1, nbV):
+        prec = chem[idV-1]
+        mini = 1000000000
+        num = -1
+        for prochain in range(nbV):
+            if (not pris[prochain]):
+                if (dist[prec][prochain] < mini):
+                    mini = dist[prec][prochain]
+                    num = prochain
+        pris[num] = True
+        chem[idV] = num
+        tot += mini
+    tot += dist[chem[nbV-1]][chem[0]]
+    return chem, tot
+
+def plotting_ville(Liste, dist):
+    '''
+    Parametre : Un liste de tuples sous la forme : (Nom de la ville , X , Y , Z ) et la distance de l'itineraire
+    exemple de liste : [('Tokyo',1,2,3),('Paris',4,6,-3),('New-York',9,-3,6),('Malaga',6,-8,7)]
+    Renvoie : None , affiche dans un espace 3D les villes via la bibliotheque matplotlib .
+    '''
+    Liste.append(Liste[0])
+    for i in range(len(Liste)-1):
+        plt.scatter(Liste[i][1],Liste[i][2], color='black')
+        plt.text(Liste[i][1],Liste[i][2],Liste[i][0], color='black', fontsize=12)
+        plt.plot([Liste[i][1],Liste[i+1][1]],[Liste[i][2],Liste[i+1][2]], color='blue')
+    plt.title("Le chemin le plus cour reliant toutes les villes fait : "+str(round(dist,3)))
+    plt.show()
+    return None
+
+def afficheRes(res, noms,coord):
+    dist = res[1]
+    ordre = res[0]
+    itineraire = []
+    for elem in ordre:
+        itineraire.append((noms[elem], coord[elem][0],coord[elem][1]))
+    plotting_ville(itineraire, dist)
+    return None
+
+        
+def main():
+    nbV, coord, noms, dist = litEntree()
+    res = glouton(nbV, coord, dist)
+    afficheRes(res,noms,coord)
+    return None
+
+
+main()
