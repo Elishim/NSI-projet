5fc36feeda942805fe8d17b68958e0c2790974dc
Elishim <96025392+Elishim@users.noreply.github.com>
2023-05-27 14:43:26 +0200

    Update affichge_graphique.py

--- affichge_graphique.py
+++ affichge_graphique.py
@@ -10,9 +10,9 @@ Liste_test=[('Tokyo',1,2),('Paris',4,6),('New-York',9,-3),('Malaga',6,-8)]
 
 def plotting_ville(Liste):
     '''
-    Parametre : Un liste de tuples sous la forme : (Nom de la ville , X , Y , Z )
-    exemple de liste : [('Tokyo',1,2,3),('Paris',4,6,-3),('New-York',9,-3,6),('Malaga',6,-8,7)]
-    Renvoie : None , affiche dans un espace 3D les villes via la bibliotheque matplotlib .
+    Parametre : Un liste de tuples sous la forme : (Nom de la ville , X , Y )
+    exemple de liste : [('Tokyo',1,2),('Paris',4,6),('New-York',9,-3),('Malaga',6,-8)]
+    Renvoie : None , affiche dans un plan les villes via la bibliotheque matplotlib .
     '''
     Liste.append(Liste[0])
     for i in range(len(Liste)-1):
