71210b6d06a583876f7aa9a78580a9d41623d0d0
Elishim <96025392+Elishim@users.noreply.github.com>
2023-05-29 15:06:16 +0200

    Update glouton.py

--- glouton.py
+++ glouton.py
@@ -93,7 +93,7 @@ def plotting_ville(Liste, dist):
     '''
     Parametre : Un liste de tuples sous la forme : (Nom de la ville , X , Y ) avec x et y des floats et le Nom de la ville un str 
     exemple de liste : [('Tokyo',1,2),('Paris',4,6),('New-York',9,-3),('Malaga',6,-8)]
-    Renvoie : None , crée un graphique reliant les villes via la bibliotheque matplotlib .
+    Renvoie : None , crée un graphique reliant les villes via la bibliotheque matplotlib et enregistre le graphique en creant l'image algo_glouton.png  .
     '''
     Liste.append(Liste[0])
     for i in range(len(Liste)-1):
@@ -101,8 +101,8 @@ def plotting_ville(Liste, dist):
         plt.text(Liste[i][1],Liste[i][2],Liste[i][0], color='black', fontsize=12)
         plt.plot([Liste[i][1],Liste[i+1][1]],[Liste[i][2],Liste[i+1][2]], color='blue')
     plt.title("Ce chemin reliant toutes les villes mesure " + str(round(dist,3)) + " km.")
-    plt.show()
     plt.savefig("algo_glouton")
+    plt.show()
     return None
 
 def afficheRes(res, noms,coord):
